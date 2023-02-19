6ba68ed0c9cafe6a00c381c63e4c5ec8561e8b04
Jaro Meyer <jaro.meyer@protonmail.com>
2023-02-19 19:03:25 +0100

    add more settings

--- mipurifier.h
+++ mipurifier.h
@@ -1,6 +1,6 @@
 #include "esphome.h"
 
-class MiPurifier : public Component, public UARTDevice, public CustomAPIDevice, public Switch {
+class MiPurifier : public Component, public UARTDevice, public CustomAPIDevice {
 public:
   static const int max_line_length = 80;
   char recv_buffer[max_line_length];
@@ -39,13 +39,28 @@ public:
   // only run setup() after a Wi-Fi connection has been established successfully
   float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; } 
 
-  void write_state(bool state) override {
-    // turn purifier on/off
-    if (state) {
-      strcpy(send_buffer, "down set_properties 2 2 true");
-    } else {
-      strcpy(send_buffer, "down set_properties 2 2 false");
-    }
+  void turn_on() {
+    strcpy(send_buffer, "down set_properties 2 2 true");
+  }
+
+  void turn_off() {
+    strcpy(send_buffer, "down set_properties 2 2 false");
+  }
+
+  void enable_beeper() {
+    strcpy(send_buffer, "down set_properties 5 1 true");
+  }
+
+  void disable_beeper() {
+    strcpy(send_buffer, "down set_properties 5 1 false");
+  }
+
+  void lock() {
+    strcpy(send_buffer, "down set_properties 7 1 true");
+  }
+
+  void unlock() {
+    strcpy(send_buffer, "down set_properties 7 1 false");
   }
 
   void set_mode(std::string mode) {
@@ -62,6 +77,16 @@ public:
       strcpy(send_buffer, "down set_properties 2 4 2");
     } else if (mode == "high") {
       strcpy(send_buffer, "down set_properties 2 4 3");
+    }
+  }
+
+  void set_brightness(std::string brightness) {
+    if (brightness == "off") {
+      strcpy(send_buffer, "down set_properties 6 1 2");
+    } else if (brightness == "low") {
+      strcpy(send_buffer, "down set_properties 6 1 1");
+    } else if (brightness == "high") {
+      strcpy(send_buffer, "down set_properties 6 1 0");
     }
   }
 
@@ -84,19 +109,19 @@ public:
       filterlife_sensor->publish_state(atof(val));
     } else if (strcmp(id, "22") == 0) {
       // power (on, off)
-      publish_state(strcmp(val, "true") == 0);
+      power_switch->publish_state(strcmp(val, "true") == 0);
     } else if (strcmp(id, "25") == 0) {
       // mode (auto, night, manual, preset)
       is_preset = false;
       switch (atoi(val)) {
         case 0:
-          mode->publish_state("auto");
+          mode_select->publish_state("auto");
           break;
         case 1:
-          mode->publish_state("night");
+          mode_select->publish_state("night");
           break;
         case 2:
-          mode->publish_state("manual");
+          mode_select->publish_state("manual");
           break;
         case 3:
           is_preset = true;
@@ -107,15 +132,34 @@ public:
       if (is_preset) {
         switch (atoi(val)) {
           case 1:
-            mode->publish_state("low");
+            mode_select->publish_state("low");
             break;
           case 2:
-            mode->publish_state("medium");
+            mode_select->publish_state("medium");
             break;
           case 3:
-            mode->publish_state("high");
+            mode_select->publish_state("high");
             break;
         }
+      }
+    } else if (strcmp(id, "51") == 0) {
+      // beeper (on, off)
+      beeper_switch->publish_state(strcmp(val, "true") == 0);
+    } else if (strcmp(id, "71") == 0) {
+      // lock (on, off)
+      lock_switch->publish_state(strcmp(val, "true") == 0);
+    } else if (strcmp(id, "61") == 0) {
+      // display brightness (off, low, high)
+      switch (atoi(val)) {
+        case 0:
+          brightness_select->publish_state("high");
+          break;
+        case 1:
+          brightness_select->publish_state("low");
+          break;
+        case 2:
+          brightness_select->publish_state("off");
+          break;
       }
     } else if (strcmp(id, "1010") == 0) {
       // manual speed
@@ -125,7 +169,8 @@ public:
 
   void setup() override {
     register_service(&MiPurifier::send_command, "send_command", {"command"});
-    strcpy(send_buffer, "down get_properties 2 2 3 6 3 7 3 8 2 5 4 3 10 10");
+    // get initial state & settings
+    strcpy(send_buffer, "down get_properties 2 2 2 5 5 1 6 1 7 1 10 10");
   }
   
   void loop() override {
@@ -135,9 +180,7 @@ public:
         if (strcmp(cmd, "net") == 0) {
             write_str("local");
         } else if (strcmp(cmd, "time") == 0) {
-          // current unix time (in seconds)
-          // TODO: send actual time
-          write_str("1669758543");
+          write_str("0");
         } else if (strcmp(cmd, "get_down") == 0) {
           // send command from send_buffer
           if (strlen(send_buffer) > 0) {
@@ -145,13 +188,15 @@ public:
             send_buffer[0] = '\0';
             ESP_LOGD("mipurifier", "sent send_buffer");
           } else if (millis() - last_heartbeat > 60000) {
+            // send mysterious heartbeat message
             write_str("down set_properties 13 9 60");
             last_heartbeat = millis();
             ESP_LOGD("purifier", "sent heartbeat");
           } else if (millis() - last_query > 60000) {
-            write_str("down get_properties 2 2 3 6 3 7 3 8 2 5 4 3 10 10");
+            // force sensor update
+            write_str("down get_properties 3 6 3 7 3 8 4 3");
             last_query = millis();
-            ESP_LOGD("purifier", "sent ");
+            ESP_LOGD("purifier", "sent query string");
           } else {
             write_str("down none");
           }
